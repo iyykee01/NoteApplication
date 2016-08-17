699ebb14a78ffba876a77ddc0b136ab265f42aa9
iyykee01 <iyykee01@gmail.com>
2016-08-17 11:58:51 +0100

    two methods to my contructor

--- noteApplication.js
+++ noteApplication.js
@@ -3,8 +3,26 @@ class NotesApplication {
 	constructor (author) {
 	
 		this.author = author;
-		this.notes = [];
+		
+		let notes = []
+		
+		/*
+		*this function takes the content as a parameter and adds it to the note list of the object.
+		*/
+		create (note_content) {
+			note.push(note_content);
+		}
+		
+		/*
+		*this function iterates through th notes array and return the index of each items as the "note_id", note_content as 'note content' and the author represent the author 
+		*/
+		listNotes () {
+			for(var i in  notes){
+				console.log('Note ID: ' + i);
+				console.log('note_CONTENT: ' + notes[i]);
+				console.log('By Author: ' + this.author);
+			}
+		}
 	}
 	
 }
-
